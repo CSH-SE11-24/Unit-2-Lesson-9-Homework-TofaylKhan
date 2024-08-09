128373eca9c068641fa40f88dde5d872883a6385
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-09 09:58:10 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// PART 1: printing with prompt
+// Prompt the user for a number and cast it to a number
+let number = prompt("Enter a number")
+
+// Prompt the user for another number that is bigger than the first number and cast it to a number
+let num1 = prompt("Enter a bigger number ")
+
+
+
+// Write a JavaScript program that uses a for loop to print all numbers from the first number to the second number
+ for (){
+  console.log(number+num1)
+}
+
+
+
+
+
+
+
+// PART 2: Averaging an array
+// Create an array of numbers 
+let nums = [2,4,6,6,7,8,2,9]
+
+
+// Write a for loop that calculates the sum of the elements 
+for 
+
+
+
+
+
+
+// Using the sum, calculate the average of all the elements (sum divided by the length of the array)
+
+
